d3d6b87acbf8dcaa619f818c582f7360fa12452f
Gualberto <gabrielgualbertooliveira@gmail.com>
2020-03-05 01:30:48 -0300

    Exercicio 5 feito

---
+++ exercicio5.c
@@ -0,0 +1,28 @@
+/*
+Aluno: Gabriel Gualberto 
+Estrutura de Dados - Fatec Ipiranga
+
+Exercicio 5
+ Altere o exercício 4 escrevendo uma função que garante que dois valores digitados pelo usuário são diferentes.
+*/
+#include<stdio.h>
+//int acha_maior (int a, int b){
+int acha_maior (){    
+    int a, b;
+    printf("Digite o primeiro numero: ");
+    scanf ("%d", &a);
+    printf("Digite o segundo numero: ");
+    scanf ("%d", &b);
+    if(a==b) acha_maior();
+    else return a > b ? a : b;
+}
+int main (){
+    /*int x=0, y;
+    printf("Digite o primeiro numero: ");
+    scanf ("%d", &x);
+    printf("Digite o segundo numero: ");
+    scanf ("%d", &y);*/
+    int r = acha_maior ();
+    printf ("Maior: %d\n", r);
+    return 0;
+}
