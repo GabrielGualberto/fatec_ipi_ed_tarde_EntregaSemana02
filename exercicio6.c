f7999491b6eed1dd96b2d22017b876b23f476065
Gualberto <gabrielgualbertooliveira@gmail.com>
2020-03-05 01:51:02 -0300

    Exercicio 6 pronto

---
+++ exercicio6.c
@@ -0,0 +1,33 @@
+/*
+Aluno: Gabriel Gualberto 
+Estrutura de Dados - Fatec Ipiranga
+
+Exercicio 6
+Escreva uma função que recebe um inteiro (1, 2 ou 3) que representa um mês do primeiro trimestre. 
+A função deve exibir o mês representado ou a mensagem “valor inválido” caso o valor seja diferente de 1, 2 e 3.
+*/
+#include<stdio.h>
+void representa_mes(int a){
+    switch (a)
+    {
+    case 1:
+        printf("Mês correspondente: Janeiro\n");
+        break;
+    case 2:
+        printf("Mês correspondente: Fevereiro\n");
+        break;
+    case 3:
+        printf("Mês correspondente: Março\n");
+        break;
+    default:
+        printf("Valor invalido\n");
+        break;
+    }
+}
+int main(){
+    int x;
+    printf("Digite um numero: \n");
+    scanf("%d",&x);
+    representa_mes(x);
+return 0;
+}
