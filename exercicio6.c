9004713edb7517eb781d5efa3ff0271feab1c40b
Gualberto <gabrielgualbertooliveira@gmail.com>
2020-03-05 01:53:19 -0300

    Exercicio 6: Com switch direto na funçao main

--- exercicio6.c
+++ exercicio6.c
@@ -7,8 +7,11 @@ Escreva uma função que recebe um inteiro (1, 2 ou 3) que representa um mês do
 A função deve exibir o mês representado ou a mensagem “valor inválido” caso o valor seja diferente de 1, 2 e 3.
 */
 #include<stdio.h>
-void representa_mes(int a){
-    switch (a)
+int main(){
+    int x;
+    printf("Digite um numero: \n");
+    scanf("%d",&x);
+    switch (x)
     {
     case 1:
         printf("Mês correspondente: Janeiro\n");
@@ -23,11 +26,5 @@ void representa_mes(int a){
         printf("Valor invalido\n");
         break;
     }
-}
-int main(){
-    int x;
-    printf("Digite um numero: \n");
-    scanf("%d",&x);
-    representa_mes(x);
 return 0;
 }
