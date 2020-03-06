1f98f1199161f4c1d5c2907974dda3c68e69ad5d
Gualberto <gabrielgualbertooliveira@gmail.com>
2020-03-05 23:56:31 -0300

    Exercicio 9 feito

---
+++ exercicio9.c
@@ -0,0 +1,20 @@
+/*
+Aluno: Gabriel Gualberto 
+Estrutura de Dados - Fatec Ipiranga
+
+Exercicio 9
+Escreva uma função que decide se um número é primo.
+*/
+#include<stdio.h>
+void vogal(char letra){
+    if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') printf("A letra %c é uma vogal", letra);
+    else  printf("A letra %c não é uma vogal", letra);
+}
+
+int main(){
+    char letra;
+    printf("Digite uma letra: ");
+    scanf("%c",&letra);
+    vogal(letra);
+return 0;
+}
