e14a39f48fb355b8976bd0c27b2c0d4579f4912b
Gualberto <gabrielgualbertooliveira@gmail.com>
2020-03-06 00:09:24 -0300

    Exercicio 9 feito

--- exercicio9.c
+++ exercicio9.c
@@ -6,6 +6,7 @@ Exercicio 9
 Escreva uma função que decide se um número é primo.
 */
 #include<stdio.h>
+#include<string.h>
 void vogal(char letra){
     if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') printf("A letra %c é uma vogal", letra);
     else  printf("A letra %c não é uma vogal", letra);
@@ -14,7 +15,8 @@ void vogal(char letra){
 int main(){
     char letra;
     printf("Digite uma letra: ");
-    scanf("%c",&letra);
+    scanf("%c",letra);
     vogal(letra);
+  
 return 0;
 }
