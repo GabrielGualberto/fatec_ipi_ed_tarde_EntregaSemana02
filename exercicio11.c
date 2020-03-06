995350a1a2e53fd68cef29ed1f0ebe31088eae43
Gualberto <gabrielgualbertooliveira@gmail.com>
2020-03-06 10:34:53 -0300

    Exercicio11 feito

---
+++ exercicio11.c
@@ -0,0 +1,36 @@
+/*
+Aluno: Gabriel Gualberto 
+Estrutura de Dados - Fatec Ipiranga
+
+Exercicio 11
+Escreva uma função que recebe um vetor de inteiros e inverte seu conteúdo. 
+Por exemplo, o vetor 1, 2, 3, 4 deve ser invertido para 4, 3, 2, 1.
+*/
+#include<stdio.h>
+void inverteVetor(int v[],int invertido[],int tamanho){
+    int i,n=tamanho;
+    for(i=0;i<tamanho;i++){
+        n--;
+        invertido[i]=v[n];
+        }
+    printf("Vetor invertido: ");
+    for (i = 0; i < tamanho; i++)
+    {
+        printf("%d ",invertido[i]);
+    }
+}
+
+int main(){
+int tamanhoVetor, i,j;
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d",&tamanhoVetor);
+int num[tamanhoVetor];
+int mun[tamanhoVetor];
+    for(i=0; i < tamanhoVetor ;i++){
+        printf("Digite um numero: ");
+        scanf("%d",&num[i]);
+    }
+    inverteVetor(num,mun,tamanhoVetor);
+
+    
+}
